3b6fdf812c42ff63621fd4a6514d7a399dc0b277
hzlllns <hello@hzl.ca>
2015-01-30 05:27:21 -0500

    Koken

---
+++ koken/storage/configuration/database.php
@@ -0,0 +1,11 @@
+<?php
+
+	$KOKEN_DATABASE = array(
+		'driver' => 'mysqli',
+		'hostname' => 'localhost',
+		'database' => 'marissf7_largo',
+		'username' => 'marissf7_koken',
+		'password' => 'dtuxA8rqXdZ',
+		'prefix' => 'koken_1',
+		'socket' => ''
+	);
---
+++ koken/storage/configuration/key.php
@@ -0,0 +1,6 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// Do not edit or remove this file unless advised by Koken support
+// support@koken.me
+
+$KOKEN_ENCRYPTION_KEY = '56471e16aaa30244e288a6d85215358e';
